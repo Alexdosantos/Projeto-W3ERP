7959946296b3bdcca0d347645195bcda37bc7162
Alex dos Santos <125704045+Alexdosantos@users.noreply.github.com>
2023-07-26 00:52:38 -0300

    Atualizado

--- src/Tour/TourDashboard.tsx
+++ src/Tour/TourDashboard.tsx
@@ -1,40 +1,76 @@
-import React from "react";
 import Tour from "reactour";
-
+import { useState } from "react";
 type StepsProps = {
   selector: string;
   content: string;
 };
 
 const steps: StepsProps[] = [
-  // ... (os passos do tour)
+  {
+    selector: "#dashboard",
+    content:
+      "Este é o painel do Dashboard, onde você pode visualizar os produtos e clientes em alta e baixa, além de obter dados percentuais e realizar verificações em tabelas sofisticadas.",
+  },
+  {
+    selector: "#predicoes",
+    content:
+      "Este é o painel de previsões no qual você pode verificar informações sobre empresas, os produtos que elas precisam comprar e as datas das próximas compras.",
+  },
+  {
+    selector: "#produtos",
+    content:
+      "Bem-vindo ao painel de produtos! Aqui você pode pesquisar por produtos de sua escolha, filtrar entre os 'em Alta' ou 'em Baixa', além de obter informações mais detalhadas sobre um produto específico ao clicar em seu nome e ser direcionado para a página de detalhes.",
+  },
+  {
+    selector: "#user-Login",
+    content:
+      "Essa é a sessão de logout e configuração do sistema , caso precise realizar alguma configuração ou sair do sistema.",
+  },
+  {
+    selector: "#dashInformation",
+    content:
+      "Bem-vindo à Dashboard Information! Aqui você terá acesso a todos os dados atualizados instantaneamente por meio de nossa infraestrutura de alta tecnologia de dados.",
+  },
+  {
+    selector: "#products-High",
+    content: "Esse é o card de informações dos produtos em Alta",
+  },
+  {
+    selector: "#products-Low",
+    content: "Esse é o card de informações dos produtos em baixa",
+  },
+  {
+    selector: "#clients-High",
+    content: "Esse é o card de informações dos clientes em Alta",
+  },
+  {
+    selector: "#clients-Low",
+    content: "Esse é o card de informações dos clientes em baixa",
+  },
+  {
+    selector: "#filter",
+    content: "Aqui você pode filtrar por mais dados em datas especificas ",
+  },
+  {
+    selector: "#contact",
+    content:
+      "Caso preciso entrar em contato conosco , basta aperta no botão Fale Conosco ",
+  },
+  {
+    selector: "#table-Information",
+    content:
+      "Bem-vindo à nossa Tabela de Informações, onde você pode clicar nos botões 'EM ALTA' OU 'EM BAIXA' para obter um filtro dos produtos mais populares ou menos populares. Além disso, você pode clicar em um produto específico para obter informações mais detalhadas sobre ele.",
+  },
 ];
-
 const TourComponent = () => {
-  const [isTourOpen, setIsTourOpen] = React.useState(true);
-  const [isNewUser, setIsNewUser] = React.useState(false); // Define como falso por padrão
-
-  React.useEffect(() => {
-    const hasViewedTour = localStorage.getItem('hasViewedTour');
-    if (!hasViewedTour) { // Verifica se o tour ainda não foi visualizado
-      setIsNewUser(true); // Define como novo usuário somente se ainda não tiver visto o tour antes
-    } else {
-      setIsTourOpen(false); // Caso já tenha visto o tour, fecha o tour automaticamente
-    }
-  }, []);
-
-  const handleCloseTour = () => {
-    localStorage.setItem('hasViewedTour', 'true');
-    setIsNewUser(false);
-  };
-
+  const [isTourOpen, setIsTourOpen] = useState(true);
   return (
     <div>
-      {isNewUser && (
+      {isTourOpen && (
         <Tour
           steps={steps}
           isOpen={isTourOpen}
-          onRequestClose={handleCloseTour}
+          onRequestClose={() => setIsTourOpen(false)}
         />
       )}
     </div>
