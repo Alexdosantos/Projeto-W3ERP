b14b4c629397f0bb51abf7dbb719a04182b6e9a9
Alex dos Santos <125704045+Alexdosantos@users.noreply.github.com>
2023-07-27 22:41:54 -0300

    Atualizado

--- src/Components/Token/Token.tsx
+++
@@ -1,7 +0,0 @@
-export type UserToken = {
-  token: string;
-};
-
-export const response: UserToken = {
-  token:"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBhcm5pYS5jb20uYnIiLCJpYXQiOjE2ODkwNDc5MjIsImV4cCI6MTY4OTEzNDMyMn0.216-L_wAP5DrITOxEGFKpGoUYYE82qb9-Fux5oq7GbVm4969UlAx_dKlxUoHEygOGtLQkzrveuVXTf64i0YFEg",
-};
--- src/Hooks/UseApi.tsx
+++ src/Hooks/UseApi.tsx
@@ -1,11 +1,10 @@
 import axios from "axios";
-import { response } from "../Components/Token/Token";
 
 export const api = axios.create({
   baseURL: "https://api.predict.app.br",
   headers: {
     "Content-Type": "application/json",
-    'Authorization': `Bearer ${response.token}`,
+    'Authorization': `Bearer ${'AUTH_TOKEN'}`,
     "X-TENANT-ID": "arnia",
   },
 });
